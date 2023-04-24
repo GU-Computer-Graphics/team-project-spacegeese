d4f93f8d9b76701b8e439a195a6efb401d54ffba
zach.burnaby <58493767+TheRedLancer@users.noreply.github.com>
2023-04-24 09:18:26 -0700

    add portal image geometry

--- src/Components/Portal.js
+++ src/Components/Portal.js
@@ -1,4 +1,4 @@
-import * as THREE from '../lib/three.module.js'
+import * as THREE from "../lib/three.module.js";
 
 const portalGeo = new THREE.TorusGeometry(4, 1);
 const portalMesh = new THREE.MeshNormalMaterial();
@@ -7,12 +7,23 @@ export default class Portal extends THREE.Group {
     constructor() {
         super();
 
-        this.geometry = portalGeo;
-        this.material = portalMesh;
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.outerGeometry = portalGeo;
+        this.outerMaterial = portalMesh;
+        this.mesh = new THREE.Mesh(this.outerGeometry, this.outerMaterial);
         this.mesh.rotateY(Math.PI / 2);
         this.add(this.mesh);
 
-        this.name = "asteroid";
+        this.portalImageGeometry = new THREE.PlaneGeometry(6, 6);
+        this.portalImageMaterial = new THREE.MeshBasicMaterial({
+            color: "green",
+        });
+        this.portalImageMesh = new THREE.Mesh(
+            this.portalImageGeometry,
+            this.portalImageMaterial
+        );
+        this.portalImageMesh.rotateY(Math.PI / 2);
+        this.add(this.portalImageMesh);
+
+        this.name = "portal";
     }
 }
